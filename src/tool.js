047b0d61e88be08910f0a9542e4abb8c24cab1f1
johnnyclapham <jmclapham@email.wm.edu>
2022-02-16 18:17:12 -0500

    #2

--- src/tool.js
+++ src/tool.js
@@ -11,15 +11,15 @@
 
 d3.select("#container")
           .transition()
-          .duration(10000)
+          .duration(1000)
           .style("background-color", "red");
 
 import { fire_config } from './fire_config.js'
 
 console.log("point1");
 
-import { initializeApp } from 'firebase/app/';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite/';
+import { initializeApp } from 'firebase/app';
+// import {initializeApp} from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-initializeApp.js'
 
 console.log(String(fire_config));
 
@@ -27,19 +27,19 @@ console.log("point2");
 
 // Initialize Firebase
 const app = initializeApp(fire_config);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
+// const analytics = getAnalytics(app);
+// const db = getFirestore(app);
 
-// // Get a list of users from your database
-// async function getUsers(db) {
-//     const users = collection(db, 'User');
-//     const userSnapshot = await getDocs(User);
-//     const userList = userSnapshot.docs.map(doc => doc.data());
-//     return userList;
-//   }
+// // // Get a list of users from your database
+// // async function getUsers(db) {
+// //     const users = collection(db, 'User');
+// //     const userSnapshot = await getDocs(User);
+// //     const userList = userSnapshot.docs.map(doc => doc.data());
+// //     return userList;
+// //   }
 
-const ref = db.ref('User');
+// const ref = db.ref('User');
 
-ref.orderByChild('height').on('child_added', (snapshot) => {
-  console.log(snapshot.key + ' :: ' + snapshot.val().latitude );
-});
+// ref.orderByChild('height').on('child_added', (snapshot) => {
+//   console.log(snapshot.key + ' :: ' + snapshot.val().latitude );
+// });

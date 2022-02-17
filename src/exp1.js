15dbf6d4018aa235a40ad7bd638958f24f55de77
johnnyclapham <jmclapham@email.wm.edu>
2022-02-17 11:37:55 -0500

    #2 and firebase testing

--- src/exp1.js
+++ src/exp1.js
@@ -1,16 +1,37 @@
 import * as d3 from 'd3';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { fire_config } from './fire_config.js'
+
 
 function app() {
+    console.log("yam has been thrown");
 
     d3.select("#container")
-          .transition()
-          .duration(1000)
-          .style("background-color", "red");
-	
+        .transition()
+        .duration(1000)
+        .style("background-color", "red");
+
+
+    const firebaseConfig = fire_config;
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
+
+    // Get a list of cities from your database
+    async function getUsers(db) {
+        const cusersCol = collection(db, 'user');
+        const userSnapshot = await getDocs(cusersCol);
+        const userList = userSnapshot.docs.map(doc => doc.data());
+        return userList;
+    }
+
+    console.log("yam has landed");
+    console.log(getUsers(db));
 }
 
 export {
-	app
+    app
 }
 
 
--- src/tool.js
+++ src/tool.js
@@ -1,9 +1,9 @@
 // Note: Tutorial with documentation https://www.tutorialsteacher.com/d3js/select-dom-element-using-d3js
 
 import {
-	app
+  app
 } from './exp1.js'
 
 app()
 
-  
+

dc9fe0646b25b5165c0b0ff38ea6885c9d69aad6
johnnyclapham <jmclapham@email.wm.edu>
2022-02-17 13:55:18 -0500

    #3 firebase is linked

--- src/exp1.js
+++ src/exp1.js
@@ -1,33 +1,31 @@
 import * as d3 from 'd3';
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getDatabase, ref, child, get } from "firebase/database";
 import { fire_config } from './fire_config.js'
-
+// Note: We are using firebase database; not firebase firestore!
 
 function app() {
-    console.log("yam has been thrown");
-
     d3.select("#container")
         .transition()
         .duration(1000)
         .style("background-color", "red");
 
-
     const firebaseConfig = fire_config;
-
     const app = initializeApp(firebaseConfig);
-    const db = getFirestore(app);
 
-    // Get a list of cities from your database
-    async function getUsers(db) {
-        const cusersCol = collection(db, 'user');
-        const userSnapshot = await getDocs(cusersCol);
-        const userList = userSnapshot.docs.map(doc => doc.data());
-        return userList;
-    }
+    // Get a reference to the database service
+    const dbRef = ref(getDatabase());
+    // Get user entry and print
+    get(child(dbRef, 'User/3')).then((snapshot) => {
+        if (snapshot.exists()) {
+            console.log(snapshot.val());
+        } else {
+            console.log("No data available");
+        }
+    }).catch((error) => {
+        console.error(error);
+    });
 
-    console.log("yam has landed");
-    console.log(getUsers(db));
 }
 
 export {

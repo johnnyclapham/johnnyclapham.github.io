907c3288c4a80f8b53362d9b313e0882a8eb1f9e
johnnyclapham <jmclapham@email.wm.edu>
2022-04-06 13:43:17 -0400

    #5 data control text now stays in its view

--- src/style.js
+++ src/style.js
@@ -3,6 +3,7 @@ function resizeView() {
     var pageWidth = window.innerWidth;
     var pageHeight = window.innerHeight;
     document.getElementById("tool_view").style.height = (0.6 * pageWidth);
+    // document.getElementById("tool_view").style.maxWidth = (pageHeight * 1);
     document.getElementById("tool_view").style.maxHeight = (0.8 * pageHeight);
 
 }
